9c8664c3949b524342b707997c536cc070999a75
Sjors Gielen <sjors@sjorsgielen.nl>
2023-08-29 13:06:00 +0200

    Fix support for handler.WithAttrs() and handler.WithGroup(). Fixes #3.

--- log.go
+++ log.go
@@ -21,3 +21,15 @@ func (t Handler) Handle(ctx context.Context, r slog.Record) error {
 	r.AddAttrs(GetAttrs(ctx)...)
 	return t.Handler.Handle(ctx, r)
 }
+
+func (t Handler) Enabled(ctx context.Context, l slog.Level) bool {
+	return t.Handler.Enabled(ctx, l)
+}
+
+func (t Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return Handler{t.Handler.WithAttrs(attrs)}
+}
+
+func (t Handler) WithGroup(name string) slog.Handler {
+	return Handler{t.Handler.WithGroup(name)}
+}
