9ebcc10c8b32301ae57d099cc344ccec365f2544
vejed <vejed@vejed.net.ru>
2024-03-13 23:11:59 +0300

    Use struct{} as context value key

--- attr.go
+++ attr.go
@@ -5,21 +5,17 @@ import (
 	"log/slog"
 )
 
-type ctxkey string
-
-const (
-	attrsKey ctxkey = "ctxlogattrs"
-)
+type ctxkey struct {}
 
 func WithAttrs(ctx context.Context, newAttrs ...slog.Attr) context.Context {
 	// Get fields if set
-	attrs, _ := ctx.Value(attrsKey).([]slog.Attr)
+	attrs, _ := ctx.Value(ctxkey{}).([]slog.Attr)
 	attrs = append(attrs, newAttrs...)
-	ctx = context.WithValue(ctx, attrsKey, attrs)
+	ctx = context.WithValue(ctx, ctxkey{}, attrs)
 	return ctx
 }
 
 func GetAttrs(ctx context.Context) []slog.Attr {
-	attrs, _ := ctx.Value(attrsKey).([]slog.Attr)
+	attrs, _ := ctx.Value(ctxkey{}).([]slog.Attr)
 	return attrs
 }
